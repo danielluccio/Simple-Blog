3f987020ab64f8cfb7d8935341b7882c7e060ac1
danielluccio <daniel130877@gmail.com>
2024-12-17 16:10:04 -0300

    Classe para Posts

--- App/admin.py
+++ App/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Posts
 
-# Register your models here.
+
+class PostsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'description')
+    search_fields = ('title', 'created_at', 'description')
+    ordering = ('-created_at',)
+
+admin.site.register(Posts, PostsAdmin)
--- App/models.py
+++ App/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Posts(models.Model):
+    title =  models.CharField(max_length=150, blank=True, null=True)
+    description = models.TextField(blank=False, null=False)
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
+    def __str__(self):
+        return f"Title: {self.title} created at {self.created_at}"
