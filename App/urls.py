1e2ba546bb0141d79a27cf21fdfba060e2bbd7e1
danielluccio <daniel130877@gmail.com>
2024-12-19 10:08:02 -0300

    Adicionando Create e ViewPost

--- App/urls.py
+++ App/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from App.views import post_list
+from App.views import post_list, post_details
 
 
 urlpatterns = [
     path('', post_list, name='post_list'),
-    
+    path('post_details/<int:id>/' , post_details, name='post_details'),
 ]
--- App/views.py
+++ App/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import Posts
+from .forms import PostForm
+from django.urls import reverse
 
 
 def post_list(request):
@@ -10,4 +13,21 @@ def post_list(request):
     }
     return render(request, template_name, context)
 
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form = form.save(commit=False)
+            return HttpResponseRedirect(reverse('post_list'))
+        
+    posts = PostForm()
+    return render(request, 'post_create', {'form': form})
 
+def post_details(request, id):
+    template_name = 'post_details.html'
+    post = Posts.objects.get(id=id)
+    context = {
+        'post': post
+    }
+
+    return render(request, template_name, context)
