f21656c5d7d14a8262afb0bade661794ecad2886
danielluccio <daniel130877@gmail.com>
2024-12-19 08:52:39 -0300

    Listandos os Posts

--- App/urls.py
+++ App/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from App.views import post_list
 
 
 urlpatterns = [
+    path('', post_list, name='post_list'),
     
 ]
--- App/views.py
+++ App/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Posts
 
-# Create your views here.
+
+def post_list(request):
+    template_name = 'post_list.html'
+    posts = Posts.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, template_name, context)
+
+
